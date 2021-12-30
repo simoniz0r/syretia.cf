2abd3ea62b8e0f5a00b09935981142427a1f5afe
Syretia <bnu.fantasma@gmail.com>
2021-12-30 05:18:41 -0600

    Run checks in parallel

--- scripts/phish.sh
+++ scripts/phish.sh
@@ -7,21 +7,25 @@ if [[ -z "$domain" ]]; then
     exit 0
 fi
 
-phisherman="$(curl -sLA 'blargbot https://blargbot.xyz' "https://api.phisherman.gg/v1/domains/$domain")"
+afdata="$(jq -n --arg d "$domain" '.message |= $d')"
+
+phisherman="$(curl -sLA 'blargbot https://blargbot.xyz' "https://api.phisherman.gg/v1/domains/$domain" &)"
+antifish="$(curl -sLX POST -H 'Content-Type: application/json' -A 'blargbot (https://blargbot.xyz)' 'https://anti-fish.bitflow.dev/check' -d "$afdata" &)"
+gsb="$(curl -sL "https://urlscan.io/api/verdict/$domain" | jq -r '.gsb.verdict.phishing?' 2>/dev/null &)"
+
+wait
+
 if [[ "$phisherman" == "true" ]]; then
     jq -cn --arg d "$domain" '.domain |= $d | .phish |= true | .source |= "phisherman.gg"'
     exit 0
 fi
 
-afdata="$(jq -n --arg d "$domain" '.message |= $d')"
-antifish="$(curl -sLX POST -H 'Content-Type: application/json' -A 'blargbot (https://blargbot.xyz)' 'https://anti-fish.bitflow.dev/check' -d "$afdata")"
 if [[ "$(echo "$antifish" | jq -r '.match')" == "true" ]]; then
     source="$(echo "$antifish" | jq -r '.matches[0].source')"
     jq -cn --arg d "$domain" --arg s "$source" '.domain |= $d | .phish |= true | .source |= $s'
     exit 0
 fi
 
-gsb="$(curl -sL "https://urlscan.io/api/verdict/$domain" | jq -r '.gsb.verdict.phishing?' 2>/dev/null)"
 if [[ "$gsb" == "true" ]]; then
     jq -cn --arg d "$domain" '.domain |= $d | .phish |= true | .source |= "Google Safe Browsing"'
     exit 0
