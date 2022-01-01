683ea8e36fec52f1516efd699218582f94064da1
Syretia <bnu.fantasma@gmail.com>
2022-01-01 03:38:32 -0600

    Check antifish results first

--- scripts/phish.sh
+++ scripts/phish.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-phisherman() {
-    curl -sLA 'blargbot https://blargbot.xyz' "https://api.phisherman.gg/v1/domains/$domain" > /home/webhookd/out/.phisherman."$timens"
-}
-
 antifish() {
     curl -sLX POST -H 'Content-Type: application/json' -A 'blargbot (https://blargbot.xyz)' 'https://anti-fish.bitflow.dev/check' -d "$afdata" > /home/webhookd/out/.antifish."$timens"
+}
+
+phisherman() {
+    curl -sLA 'blargbot https://blargbot.xyz' "https://api.phisherman.gg/v1/domains/$domain" > /home/webhookd/out/.phisherman."$timens"
 }
 
 gsb() {
@@ -22,28 +22,28 @@ fi
 export timens="$(date +%s%N)"
 export afdata="$(jq -n --arg d "$domain" '.message |= $d')"
 
-phisherman &
 antifish &
+phisherman &
 gsb &
 
 wait
 
-phisherman="$(cat /home/webhookd/out/.phisherman."$timens")"
 antifish="$(cat /home/webhookd/out/.antifish."$timens")"
+phisherman="$(cat /home/webhookd/out/.phisherman."$timens")"
 gsb="$(cat /home/webhookd/out/.gsb."$timens")"
 
-rm -rf /home/webhookd/out/.phisherman."$timens"
 rm -rf /home/webhookd/out/.antifish."$timens"
+rm -rf /home/webhookd/out/.phisherman."$timens"
 rm -rf /home/webhookd/out/.gsb."$timens"
-
-if [[ "$phisherman" == "true" ]]; then
-    jq -cn --arg d "$domain" '.domain |= $d | .phish |= true | .source |= "phisherman.gg" | .raw |= true'
-    exit 0
-fi
 
 if [[ "$(echo "$antifish" | jq -r '.match')" == "true" ]]; then
     source="anti-fish.bitflow.dev ($(echo "$antifish" | jq -r '.matches[0].source'))"
     jq -cn --arg d "$domain" --arg s "$source" --argjson r "$antifish" '.domain |= $d | .phish |= true | .source |= $s | .raw |= $r'
+    exit 0
+fi
+
+if [[ "$phisherman" == "true" ]]; then
+    jq -cn --arg d "$domain" '.domain |= $d | .phish |= true | .source |= "phisherman.gg" | .raw |= true'
     exit 0
 fi
 
