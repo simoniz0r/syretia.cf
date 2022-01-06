910ec352bc3e8b3bde400ae7e40a5211c3188376
Syretia <bnu.fantasma@gmail.com>
2022-01-06 10:01:08 -0600

    Remove dialogpt.sh

--- scripts/dialogpt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ -z "$model" ]]; then
-    model="facebook/blenderbot-400M-distill"
-fi
-
-curl -sL "https://api-inference.huggingface.co/models/$model" -H "Authorization: Bearer $token" -H 'content-type: application/json' --data-raw "$@"
