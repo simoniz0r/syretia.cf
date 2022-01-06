800e70b5d479e8ac5d1415bb8896af2fa2a3bc6d
Syretia <bnu.fantasma@gmail.com>
2022-01-06 09:57:38 -0600

    Remove wikiaotd.sh

--- scripts/wikiaotd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-curl -sL 'https://en.wikipedia.org/wiki/Wikipedia:Today%27s_featured_article' 2>/dev/null | pup 'a:contains("Full") attr{href}' | grep -v '/wiki/2008_Orange_Bowl' | head -n 1
