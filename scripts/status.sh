b8abb4392b83bf24febcaa3a88c89890f1065fe7
Syretia <bnu.fantasma@gmail.com>
2022-01-08 02:24:35 -0600

    Output headers as array

--- scripts/status.sh
+++ scripts/status.sh
@@ -10,6 +10,6 @@ fi
 REQ="$(/home/syretia/curl -sLIX HEAD -w '\n%{json}\n' --max-time 7 -A "$RANDOM$RANDOM" "$url" | tac || true)"
 
 json="$(echo "$REQ" | head -n 1)"
-headers="$(echo "$REQ" | tail -n +2 | tac)"
+headers="$(echo "$REQ" | tail -n +2 | tac | jq -Rs 'split("\r\n") | .[0:-2]')"
 
-echo "$json" | jq --sort-keys --rawfile h <(echo -n "$headers") '.headers |= $h | .local_ip |= null'
+echo "$json" | jq --sort-keys --argfile h <(echo -n "$headers") '.headers |= $h | .local_ip |= null'
