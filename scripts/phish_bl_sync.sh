479ab84b119179a197ea5d0439d41acc2e5bfc9d
Syretia <bnu.fantasma@gmail.com>
2022-01-10 09:14:13 -0600

    Only get domains added in last $dt seconds

--- scripts/phish_bl_sync.sh
+++ scripts/phish_bl_sync.sh
@@ -9,10 +9,14 @@ fi
 
 # if no POST data, sync local database to https://phish.sinking.yachts/v2/all
 if [[ -z "$@" ]]; then
-  # get list of phishing domains from phish.sinking.yachts and output to /home/webhookd/out/.yachts
-  curl -sL 'https://phish.sinking.yachts/v2/all' -H 'X-Identity: Syretia (https://syretia.cf/docs)' -o /home/webhookd/out/.yachts
+  if [[ -z "$dt" ]]; then
+    dt="3600"
+  fi
+  # get list of phishing domains added in the last hour from phish.sinking.yachts and output to /home/webhookd/out/.yachts
+  curl -sL "https://phish.sinking.yachts/v2/recent/$dt" -H 'X-Identity: Syretia (https://syretia.cf/docs)' | \
+  | jq -c '[.[].domains[]]' 2>/dev/null > /home/webhookd/out/.yachts
   # check if file exists and is valid JSON
-  if [[ -f "/home/webhookd/out/.yachts" ]] && jq '.' /home/webhookd/out/.yachts &>/dev/null; then
+  if [[ -f "/home/webhookd/out/.yachts" && "$(jq 'length' /home/webhookd/out/.yachts 2>/dev/null || echo -n '0')" != "0" ]]; then
           # combine /home/webhookd/out/.yachts and /home/webhookd/jsonlite/domains/blacklist
           # then sort, filter out duplicates, and filter out entries that start with 'www.'
           cat /home/webhookd/out/.yachts | jq -c --argfile bl /home/webhookd/jsonlite/domains/blacklist \
