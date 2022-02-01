9787125b826a8202a63a681ca0470cab24824b83
Syretia <bnu.fantasma@gmail.com>
2022-01-31 18:45:20 -0600

    Change user-agent to $RANDOM$RANDOM

--- scripts/metadata.sh
+++ scripts/metadata.sh
@@ -3,5 +3,10 @@
 rm -rf /home/webhookd/logs/*
 
 if [[ -n "$u" ]]; then
-	curl --max-time 2.5 -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -sL "$u" | pup 'meta json{}' 2>&1 | perl -pe 's%^EOF$%\[\]%' | perl -pe 's%("property":|"http-equiv":)%"name":%g' | perl -pe 's%"value":%"content":%g' | jq --sort-keys 'walk(if type == "object" then del(.tag) else . end) | sort_by(.name)'
+	curl --max-time 2.5 -A "$RANDOM$RANDOM" -sL "$u" | \
+	pup 'meta json{}' 2>&1 | \
+	perl -pe 's%^EOF$%\[\]%' | \
+	perl -pe 's%("property":|"http-equiv":)%"name":%g' | \
+	perl -pe 's%"value":%"content":%g' | \
+	jq --sort-keys 'walk(if type == "object" then del(.tag) else . end) | sort_by(.name)'
 fi
--- scripts/motyar.sh
+++ scripts/motyar.sh
@@ -4,5 +4,5 @@ rm -rf /home/webhookd/logs/*
 
 if [[ -n "$u" && -n "$p" ]]; then
 	p="$p json{}"
-	curl --max-time 2.5 -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36" -sL "$u" | pup "$p" 2>&1 | perl -pe 's%^EOF$%\[\]%'
+	curl --max-time 2.5 -A "$RANDOM$RANDOM" -sL "$u" | pup "$p" 2>&1 | perl -pe 's%^EOF$%\[\]%'
 fi
