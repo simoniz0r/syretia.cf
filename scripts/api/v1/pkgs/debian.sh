8f6f11b29476668ec145af4d0b30b8e4e7ae7c82
simoniz0r <bnu.fantasma@gmail.com>
2024-05-01 07:40:54 +0000

    Allow release to be set with url query

--- scripts/api/v1/pkgs/debian.sh
+++ scripts/api/v1/pkgs/debian.sh
@@ -5,16 +5,25 @@
 # Dependencies: curl, jq, pup
 # Description: Debian package search
 
-pkg_name="$(curl -sL "https://packages.debian.org/search?suite=bookworm&searchon=names&keywords=$q" | \
+# support old query variable
+if [[ -n "$q" ]]; then
+	query="$q"
+fi
+# set release to bookworm if not set
+if [[ -z "$release" ]]; then
+	release="bookworm"
+fi
+# scrape package name from search results
+pkg_name="$(curl -sL "https://packages.debian.org/search?suite=$release&searchon=names&keywords=$query" | \
 pup 'a attr{href}' | \
-grep -m1 '^/bookworm/' | \
+grep -m1 "^/$release/" | \
 cut -f3 -d'/')"
-
+# exit if not found
 if [[ -z "$pkg_name" ]]; then
 	exit 0
 fi
-
-curl -sL "https://packages.debian.org/bookworm/$pkg_name" | \
+# get metadata from package page which has version, description, etc
+curl -sL "https://packages.debian.org/$release/$pkg_name" | \
 pup 'meta json{}' | \
 jq --arg pn "$pkg_name" '{name: $pn, description: .[2].content, info: .[3].content, author: .[1].content}'
 rm -rf /home/webhookd/logs/*
--- scripts/api/v1/pkgs/ubuntu.sh
+++ scripts/api/v1/pkgs/ubuntu.sh
@@ -5,16 +5,25 @@
 # Dependencies: curl, jq, pup
 # Description: Ubuntu package search
 
-pkg_name="$(curl -sL "https://packages.ubuntu.com/search?suite=noble&searchon=names&keywords=$q" | \
+# support old query variable
+if [[ -n "$q" ]]; then
+	query="$q"
+fi
+# set release to bookworm if not set
+if [[ -z "$release" ]]; then
+	release="$release"
+fi
+# scrape package name from search results
+pkg_name="$(curl -sL "https://packages.ubuntu.com/search?suite=$release&searchon=names&keywords=$query" | \
 pup 'a attr{href}' | \
-grep -m1 '^/noble/' | \
+grep -m1 "^/$release/" | \
 cut -f3 -d'/')"
-
+# exit if not found
 if [[ -z "$pkg_name" ]]; then
 	exit 0
 fi
-
-curl -sL "https://packages.ubuntu.com/noble/$pkg_name" | \
+# get metadata from package page which has version, description, etc
+curl -sL "https://packages.ubuntu.com/$release/$pkg_name" | \
 pup 'meta json{}' | \
 jq --arg pn "$pkg_name" '{name: $pn, description: .[2].content, info: .[3].content, author: .[1].content}'
 rm -rf /home/webhookd/logs/*
