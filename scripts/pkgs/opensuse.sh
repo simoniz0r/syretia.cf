0dc3df70cc78d887c81e8f1cbc3d9f99f4265f1e
simoniz0r <bnu.fantasma@gmail.com>
2024-05-06 06:34:22 +0000

    Change q variable to query

--- scripts/pkgs/opensuse.sh
+++ scripts/pkgs/opensuse.sh
@@ -5,16 +5,25 @@
 # Dependencies: curl, oq
 # Description: openSUSE package search
 
+# remove logs
 rm -rf /home/webhookd/logs/*
 
-res="$(curl -sL "https://opi-proxy.opensuse.org/?obs_api_link=https%3A%2F%2Fapi.opensuse.org%2Fsearch%2Fpublished%2Fbinary%2Fid%3Fmatch%3D%2540name%253D%2527$q%2527%26limit%3D0&obs_instance=openSUSE" | \
+# set query to q if present
+if [[ -n "$q" ]]; then
+    query="$q"
+fi
+
+# try searching for exact match first
+res="$(curl -sL "https://opi-proxy.opensuse.org/?obs_api_link=https%3A%2F%2Fapi.opensuse.org%2Fsearch%2Fpublished%2Fbinary%2Fid%3Fmatch%3D%2540name%253D%2527$query%2527%26limit%3D0&obs_instance=openSUSE" | \
 oq -i xml -o json '[.collection.binary[] | select(."@baseproject" == "openSUSE:Factory") | select(."@arch" | test("(x86_64|noarch)"))]' 2>/dev/null)"
 
+# try fuzzy search if no exact match
 if [[ "$res" == "" ]]; then
-    res="$(curl -sL "https://opi-proxy.opensuse.org/?obs_api_link=https%3A%2F%2Fapi.opensuse.org%2Fsearch%2Fpublished%2Fbinary%2Fid%3Fmatch%3Dcontains-ic%2528%2540name%252C%2B%2527$q%2527%2529%2B%26limit%3D0&obs_instance=openSUSE" | \
+    res="$(curl -sL "https://opi-proxy.opensuse.org/?obs_api_link=https%3A%2F%2Fapi.opensuse.org%2Fsearch%2Fpublished%2Fbinary%2Fid%3Fmatch%3Dcontains-ic%2528%2540name%252C%2B%2527$query%2527%2529%2B%26limit%3D0&obs_instance=openSUSE" | \
     oq -i xml -o json '[.collection.binary[] | select(."@baseproject" == "openSUSE:Factory") | select(."@arch" | test("(x86_64|noarch)"))]' 2>/dev/null)"
 fi
 
+# set values to null and exit if no results
 if [[ "$res" == "" ]]; then
     oq -n '."@name" |= null |
     ."@project" |= null |
@@ -30,6 +39,7 @@ if [[ "$res" == "" ]]; then
     exit 0
 fi
 
+# if results contain build for Factory, output that, otherwise output first result
 if [[ "$(echo "$res" | oq '[.[] | select(."@project" == "openSUSE:Factory")] | .[0]')" != "null" ]]; then
     echo "$res" | oq '[.[] | select(."@project" == "openSUSE:Factory")] | .[0]'
 else
