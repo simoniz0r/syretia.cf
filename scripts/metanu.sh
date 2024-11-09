e16764127a1c505c0d52bca36d3a38ed8cc0d7f9
simoniz0r <bnu.fantasma@gmail.com>
2024-11-09 14:43:37 +0000

    Add comments and cleanup

--- scripts/metanu.sh
+++ scripts/metanu.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env nu
+# Name: metanu
+# Author: Syretia
+# License: MIT
+# Dependencies: nushell, nushell query plugin
+# Description: output metadata for a given url in JSON format
 
-# plugin add ./nu_plugin_query
+# use query plugin for webpage-info
 plugin use query
 
+# http get url, query webpage-info for metadata, output as json
 try { http get -m 10 $env.u | query webpage-info | to json -r } catch { |e| $e.msg }
--- scripts/reqnu.sh
+++ scripts/reqnu.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env nu
+# Name: reqnu
+# Author: Syretia
+# License: MIT
+# Dependencies: nushell, bcrypt-tool
+# Description: gets headers and body that were set by blargbot's request and reroutes them to the given url
 
 source /home/syretia/git/syretia.cf/scripts/.env.nu
 
 def main [...body] {
     # get credentials
-    # let hash = try { $env.x_authorization_hash } catch { |e| return $e.msg }
     let token = try { $env.x_authorization } catch { |e| return $e.msg }
     # check hash against token
     let hash_check = try { /usr/local/bin/bcrypt-tool match $"($token)" $"($hash)" | complete | get stdout | str replace -ra "\n" "" } catch { |e| return $e.msg }
+    # reset token variable
+    let token = ''
     #check if match
     if $hash_check != "yes" {
         return "Hash does not match"
@@ -16,6 +22,7 @@ def main [...body] {
     let method = try { $env.hook_method } catch { |e| return $e.msg }
     let url = try { $env.url } catch { |e| return $e.msg }
     let headers = $env.x_headers? | default '["user-agent", "nushell"]'
+	# run http command based on method and output result as json
     match $method {
         GET => { try { http get -fem 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } }
         POST => { try { http post -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } }
@@ -24,4 +31,5 @@ def main [...body] {
         DELETE => { try { http delete -fem 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } }
         HEAD => { try { http head -m 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } }
     } | str replace -ra '7[0-9].[0-9][0-9]?[0-9]?.[0-9][0-9]?[0-9]?.[0-9][0-9]?[0-9]?' 'syretia.xyz'
+    # replace ip in all outputs
 }
