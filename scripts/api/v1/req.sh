6eb846ecdfc55da84f994eed822dd753ffe4637a
simoniz0r <bnu.fantasma@gmail.com>
2024-11-10 20:20:05 +0000

    Simplify hash check

--- scripts/api/v1/req.sh
+++ scripts/api/v1/req.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nu
-# Name: req
+# Name: reqnu
 # Author: Syretia
 # License: MIT
 # Dependencies: nushell
@@ -11,13 +11,7 @@ def main [...body] {
     # get ip hash
     let ip_hash = try { $env.cf_connecting_ip | hash sha256 } catch { |e| return $e.msg }
     # check hash against stored hashes
-    if $ip_hash == $blargbot {
-        sleep 0sec
-    } else if $ip_hash == $bakone {
-        sleep 0sec
-    } else if $ip_hash == $baktwo {
-        sleep 0sec
-    } else {
+    if $ip_hash not-in [$blargbot $bakone $baktwo] {
         return "Unauthorized use"
     }
     # get http request info
@@ -26,12 +20,12 @@ def main [...body] {
     let headers = $env.x_headers? | default '["user-agent", "nushell"]'
     # run http command based on method and output result as json
     match $method {
-        GET => { try { http get -fem 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } }
-        POST => { try { http post -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } }
-        PATCH => { try { http patch -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } }
-        PUT => { try { http put -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } }
-        DELETE => { try { http delete -fem 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } }
-        HEAD => { try { http head -m 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } }
+        GET => { try { http get -fem 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } },
+        POST => { try { http post -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } },
+        PATCH => { try { http patch -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } },
+        PUT => { try { http put -fem 50 -H ($headers | from json) $url ($body | get 0) | to json -r } catch { |e| return $e.msg } },
+        DELETE => { try { http delete -fem 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } },
+        HEAD => { try { http head -m 50 -H ($headers | from json) $url | to json -r } catch { |e| return $e.msg } },
     } | str replace -ra '7[0-9].[0-9][0-9][0-9].[0-9].[0-9][0-9]' 'syretia.xyz'
     # replace ip in all outputs
 }
