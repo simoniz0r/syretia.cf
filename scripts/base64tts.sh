3242cbb59feff3441d241c544d8d93c0af6e3d32
simoniz0r <bnu.fantasma@gmail.com>
2024-12-02 12:01:15 +0000

    Add cursecode function

--- scripts/base64tts.sh
+++ scripts/base64tts.sh
@@ -3,16 +3,21 @@
 # Author: Syretia
 # License: MIT
 # Dependencies: nushell
-# Description: fetches TTS URLs from a couple of free services and returns the result encoded in base64 format
+# Description: fetches TTS audio from a couple of free services and returns the result encoded in base64 format
 
 # get TTS audio from lazypy
 def lazypy [voice service text] {
     # create body using url build-query
     let body = $text | wrap text | merge ($service | wrap service) | merge ($voice | wrap voice) | url build-query
     # post body to lazypy and get audio_url
-    let audio_url = try { http post -m 10 -H [content-type application/x-www-form-urlencoded] https://lazypy.ro/tts/request_tts.php $body | get audio_url } catch { |e| return ($e.msg | wrap error) }
+    let audio_json = try { http post -m 10 -H [content-type application/x-www-form-urlencoded] https://lazypy.ro/tts/request_tts.php $body } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
+    # return response if success is not true
+    if ($audio_json | get success) != true {
+        return ($audio_json | wrap error)
+    }
+    let audio_url = try { $audio_json | get audio_url } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
     # fetch and base64 encode result
-    let base64 = try { http get -m 5 $audio_url | encode base64 } catch { |e| return ($e.msg | wrap error) }
+    let base64 = try { http get -m 5 $audio_url | encode base64 } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
     return ($base64 | prepend "data:audio/mp3;base64," | str join "" | wrap audioUrl | merge ($audio_url | wrap originalUrl))
 }
 
@@ -21,9 +26,20 @@ def weilbyte [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request and bas64 encode result
-    let base64 = try { http post -m 6 -H [content-type application/json] https://tiktok-tts.weilbyte.dev/api/generate $body | encode base64 } catch { return (gesserit $voice $text) }
+    let base64 = try { http post -m 6 -H [content-type application/json] https://tiktok-tts.weilbyte.dev/api/generate $body | encode base64 } catch { return (cursecode $voice $text) }
     # wrap result for json otuput
     return ($base64 | prepend "data:audio/mp3;base64," | str join "" | wrap audioUrl)
+}
+
+# get TikTok TTS audio from cursecode
+def cursecode [voice text] {
+    # setup body json
+    let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
+    # make http post request
+    let req_json = try { http post -m 6 -H [content-type application/json] https://tts.cursecode.me/api/tts $body } catch { return (gesserit $voice $text) }
+    let json = try { $req_json | rename -c { audio: audioUrl } } catch { return (gesserit $voice $text) }
+    # output json result
+    return $json
 }
 
 # get TikTok TTS audio from gesserit
@@ -41,9 +57,10 @@ def uberduck [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request
-    let audio_url = try { http post -m 10 -H [content-type application/json] https://www.uberduck.ai/splash-tts $body | get response.path } catch { |e| return ($e.msg | wrap error) }
+    let audio_json = try { http post -m 10 -H [content-type application/json] https://www.uberduck.ai/splash-tts $body } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
+    let audio_url = try { $audio_json | get response.path } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
     # fetch and base64 encode result
-    let base64 = try { http get -m 5 $audio_url | encode base64 } catch { |e| return ($e.msg | wrap error) }
+    let base64 = try { http get -m 5 ($audio_url) | encode base64 } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
     # wrap result for json otuput
     return ($base64 | prepend "data:audio/wav;base64," | str join "" | wrap audioUrl | merge ($audio_url | wrap originalUrl))
 }
@@ -53,7 +70,7 @@ def audiourl [voice text] {
     # url encode text and prepend url
     let enc_url = $text | url encode | prepend $voice | str join ""
     # fetch and base64 encode audio url
-    let base64 = try { http get -m 5 $enc_url | encode base64 } catch { |e| return $e.msg }
+    let base64 = try { http get -m 5 $enc_url | encode base64 } catch { |e| return ($e.json | from json | select msg labels | wrap error) }
     # wrap result for json output
     return ($base64 | prepend "data:audio/mp3;base64," | str join "" | wrap audioUrl | merge ($enc_url | wrap originalUrl))
 }
