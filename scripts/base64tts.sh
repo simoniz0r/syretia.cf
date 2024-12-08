c09a1a9d123b56c73abcc6af76e9ea3447b00cca
simoniz0r <bnu.fantasma@gmail.com>
2024-12-08 11:27:48 +0000

    Add quotes around urls

--- scripts/base64tts.sh
+++ scripts/base64tts.sh
@@ -20,7 +20,7 @@ def weilbyte [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request and bas64 encode result, fallback to cursecode if fails
-    let base64 = try { http post -m 6 -H [content-type application/json] https://tiktok-tts.weilbyte.dev/api/generate $body | encode base64 } catch { return (weilnet $voice $text) }
+    let base64 = try { http post -m 6 -H [content-type application/json] "https://tiktok-tts.weilbyte.dev/api/generate" $body | encode base64 } catch { return (weilnet $voice $text) }
     # wrap result for json otuput
     return ($base64 | prepend "data:audio/mp3;base64," | str join "" | wrap audioUrl)
 }
@@ -30,7 +30,7 @@ def weilnet [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request, fallback to weilbyte
-    let req_json = try { http post -m 6 -H [content-type application/json] https://tiktok-tts.weilnet.workers.dev/api/generation $body } catch { return (cursecode $voice $text) }
+    let req_json = try { http post -m 6 -H [content-type application/json] "https://tiktok-tts.weilnet.workers.dev/api/generation" $body } catch { return (cursecode $voice $text) }
     # prepend audio info and rename column
     let json = try { $req_json | upsert data { |row| $row.data | prepend "data:audio/mp3;base64," | str join "" } | rename -c {data: audioUrl} } catch { return (cursecode $voice $text) }
     # output json result
@@ -42,7 +42,7 @@ def cursecode [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request, fallback to gesserit if fails
-    let req_json = try { http post -m 6 -H [content-type application/json] https://tts.cursecode.me/api/tts $body } catch { return (gesserit $voice $text) }
+    let req_json = try { http post -m 6 -H [content-type application/json] "https://tts.cursecode.me/api/tts" $body } catch { return (gesserit $voice $text) }
     let json = try { $req_json | rename -c { audio: audioUrl } } catch { return (gesserit $voice $text) }
     # output json result
     return $json
@@ -53,7 +53,7 @@ def gesserit [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request, fallback to lazypy if fails
-    let json = try { http post -m 6 https://gesserit.co/api/tiktok-tts $body } catch { return (lazypy $voice "TikTok" $text) }
+    let json = try { http post -m 6 "https://gesserit.co/api/tiktok-tts" $body } catch { return (lazypy $voice "TikTok" $text) }
     # output json result
     return $json
 }
@@ -63,7 +63,7 @@ def uberduck [voice text] {
     # setup body json
     let body = $text | wrap text | merge ($voice | wrap voice) | to json -r
     # make http post request
-    let audio_json = try { http post -m 10 -H [content-type application/json] https://www.uberduck.ai/splash-tts $body } catch { |e| return ($e.json | from json | wrap error) }
+    let audio_json = try { http post -m 10 -H [content-type application/json] "https://www.uberduck.ai/splash-tts" $body } catch { |e| return ($e.json | from json | wrap error) }
     let audio_url = try { $audio_json | get response.path } catch { |e| return ($e.json | from json | wrap error) }
     # fetch and base64 encode result
     let base64 = try { http get -m 5 ($audio_url) | encode base64 } catch { |e| return ($e.json | from json | wrap error) }
@@ -76,7 +76,7 @@ def lazypy [voice service text] {
     # create body using url build-query
     let body = $text | wrap text | merge ($service | wrap service) | merge ($voice | wrap voice) | url build-query
     # post body to lazypy and get audio_url
-    let audio_json = try { http post -m 10 -H [content-type application/x-www-form-urlencoded] https://lazypy.ro/tts/request_tts.php $body } catch { |e| return ($e.json | from json | wrap error) }
+    let audio_json = try { http post -m 10 -H [content-type application/x-www-form-urlencoded] "https://lazypy.ro/tts/request_tts.php" $body } catch { |e| return ($e.json | from json | wrap error) }
     # return response if success is not true
     if ($audio_json | get success) != true {
         return ($audio_json | wrap error)
