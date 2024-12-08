49c3d93060156590c2912dfd8f7c1702039f7b31
simoniz0r <bnu.fantasma@gmail.com>
2024-12-08 13:48:11 +0000

    Better error messages

--- scripts/nopaste.sh
+++ scripts/nopaste.sh
@@ -10,11 +10,19 @@ def main [...body] {
     let in_paste = $env.p? | default ""
     if $in_paste != "" {
         # try to base64 decode input
-        let decoded = try { $in_paste | base64 -dw0 | complete } catch { |e| return ($e.msg | wrap error | to json -r) }
+        let decoded = try {
+            $in_paste | base64 -dw0 | complete
+        } catch {
+            |e| return ("Failed to decode paste" | wrap error | to json -r)
+        }
         # return if exit not 0
         if ($decoded | get exit_code) != 0 { return ($decoded | wrap error | to json -r) }
         # try to decompress decoded input
-        let decompressed = try { $decoded | get stdout | lzma -d | complete } catch { |e| return ($e.msg | wrap error | to json -r) }
+        let decompressed = try {
+            $decoded | get stdout | lzma -d | complete
+        } catch {
+            |e| return ("Failed to decompress paste" | wrap error | to json -r)
+        }
         # return if exit not 0
         if ($decompressed | get exit_code) != 0 { return ($decompressed | wrap error | to json -r) }
         # get stdout from complete output
@@ -22,9 +30,17 @@ def main [...body] {
     } else {
         # url does not include paste
         # try to get paste from body
-        let body_paste = try { $body | get 0 | from json | get paste } catch { |e| return ($e.msg | wrap error | to json -r) }
+        let body_paste = try {
+            $body | get 0 | from json | get paste
+        } catch {
+            return ("Failed to parse body" | wrap error | to json -r)
+        }
         # try to compress and base64 encode paste
-        let lzma_base64 = try { $body_paste | lzma | base64 -w0 | complete } catch { |e| return ($e.msg | wrap error | to json -r) }
+        let lzma_base64 = try {
+            $body_paste | lzma | base64 -w0 | complete
+        } catch {
+            |e| return ("Failed to compress and encode paste" | wrap error | to json -r)
+        }
         # return if exit not 0
         if ($lzma_base64 | get exit_code) != 0 { return ($lzma_base64 | wrap error | to json -r) }
         # check if url includes language
@@ -36,7 +52,7 @@ def main [...body] {
             merge ($lzma_base64 | get stdout | url encode | url encode | wrap base64_url_enc) | 
             to json -r
         )} catch {
-            |e| return ($e | wrap error | to json -r)
+            |e| return ("Failed to create response JSON" | wrap error | to json -r)
         }
         # output json
         return $json
